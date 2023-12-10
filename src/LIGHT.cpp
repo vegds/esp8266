b8b7f0460c06d83f482401b79b894f8f8653755d
vegds <504136389@qq.com>
2023-12-10 13:30:52 +0800

    fix up

--- src/LIGHT.cpp
+++ src/LIGHT.cpp
@@ -6,187 +6,273 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <ESP8266WiFi.h>
-#include <WiFiManager.h>  
+#include <WiFiManager.h>
 
-char auth[] = "fe2d715bf9a0"; 
+char auth[] = "fe2d715bf9a0";
 // 获取当前时间
 unsigned long startTime;
-WiFiManager wifiManager;
-uint8_t progress = 0; // 进度条的当前进度
+WiFiManager wifiManager1;
+
+uint8_t progress = 4; // 进度条的当前进度
 const int relayPin = D6;
 
 bool relayState = HIGH;
 
 //* NeoPixel配置
-#define PIN  15              //  DIN PIN (GPIO15, D8)
-#define NUMPIXELS 11         // Number of you led
+#define PIN 15       //  DIN PIN (GPIO15, D8)
+#define NUMPIXELS 17// Number of you led
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE); 
-/*blinker定义组件*/ 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+/*blinker定义组件*/
 BlinkerRGB Color("Color");
 BlinkerSlider Slider1("bright");
 BlinkerButton mode("power");
 
-int LED_R = 0, LED_G = 0, LED_B = 0;  // RGB
+uint32_t colors[] = {pixels.Color(255, 0, 0), pixels.Color(0, 255, 0), pixels.Color(0, 0, 255)};
+
+int LED_R = 0, LED_G = 0, LED_B = 0; // RGB
 bool WIFI_Status;
-char* hex_color="#000000";
-int currentPage = 0;
+char *hex_color = "#000000";
+int page = 0;
 unsigned long previousMillis = 0;
-const long interval = 5000; // 切换页面的间隔时间，单位为毫秒
-    
-//初始化ws2812
-void initws2812(){
+unsigned long interval = 10;
+bool change = false;
+
+// 初始化ws2812
+void initws2812()
+{
   pixels.begin();
   pixels.show();
 }
 
-char* rgbToHex(uint8_t r, uint8_t g, uint8_t b) {
-  char* hex = new char[7];
+char *rgbToHex(uint8_t r, uint8_t g, uint8_t b)
+{
+  char *hex = new char[7];
   sprintf(hex, "#%02X%02X%02X", r, g, b);
   return hex;
 }
 
-//设置灯的颜色
-void SetColor(int R, int G, int B) {
-hex_color=rgbToHex(R, G, B);
-  for (uint16_t i = 0; i < NUMPIXELS; i++)  //把灯条变色
+// 设置灯的颜色
+void SetColor(int R, int G, int B)
+{
+  change = true;
+  hex_color = rgbToHex(R, G, B);
+  for (uint16_t i = 0; i < NUMPIXELS; i++) // 把灯条变色
   {
     pixels.setPixelColor(i, R, G, B);
   }
-  pixels.show();                 //送出显示
+  pixels.show(); // 送出显示
 }
 
-//设置灯的亮度
-void SetBright(int bright) { 
-  pixels.setBrightness(bright);
+// 设置灯的亮度
+void SetBright(int bright)
+{
+  pixels.setBrightness(bright * 84+1);
   pixels.show();
 }
 
 void connect()
-{ 
-    // 自动连接WiFi。以下语句的参数是连接ESP8266时的WiFi名称
-    wifiManager.autoConnect("vegds","12345678");
+{
+  // 自动连接WiFi。以下语句的参数是连接ESP8266时的WiFi名称
+  wifiManager1.setEnableConfigPortal(false);
+  wifiManager1.autoConnect();
+  wifiManager1.setParamsPage(true);
 }
 
-//APP RGB颜色设置回调
-void rgb1_callback(uint8_t r_value, uint8_t g_value, uint8_t b_value, uint8_t bright) {
-
-  //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+// APP RGB颜色设置回调
+void rgb1_callback(uint8_t r_value, uint8_t g_value, uint8_t b_value, uint8_t bright)
+{
+  // digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   BLINKER_LOG("R value: ", r_value);
   BLINKER_LOG("G value: ", g_value);
   BLINKER_LOG("B value: ", b_value);
   SetColor(r_value, g_value, b_value);
 }
 
-void slider_callback(int32_t bright) {
+void slider_callback(int32_t bright)
+{
   BLINKER_LOG("bright value: ", bright);
   SetBright(bright);
 }
 
-void drawProgressBar(int progress) {
-  int width = u8g2.getWidth() - 2; // 进度条宽度
-  int height = 10; // 进度条高度
-  int startX = 1; // 进度条起始X坐标
+void drawProgressBar(int progress)
+{
+  int width = u8g2.getWidth() - 2;                // 进度条宽度
+  int height = 10;                                // 进度条高度
+  int startX = 1;                                 // 进度条起始X坐标
   int startY = u8g2.getHeight() / 2 - height / 2; // 进度条起始Y坐标
-  u8g2.drawFrame(startX, startY, width, height); // 绘制进度条边框
-  u8g2.drawUTF8(40, startY+22, "正在启动");
+  u8g2.drawFrame(startX, startY, width, height);  // 绘制进度条边框
+  u8g2.drawUTF8(40, startY + 22, "正在启动");
   u8g2.drawBox(startX + 1, startY + 1, progress * (width - 2) / 100, height - 2); // 绘制进度条填充
-  u8g2.drawUTF8(15, startY-12, "by:光与影太遥远");
-  u8g2.drawUTF8(30, startY+33, "github:vegds");
+  u8g2.drawUTF8(15, startY - 12, "by:光与影太遥远");
+  u8g2.drawUTF8(30, startY + 33, "github:vegds");
   u8g2.sendBuffer();
   delay(1000);
 }
 
-
-//灯的模式回调
-void mode_callback(const String& state)
+// 灯的模式回调
+void mode_callback(const String &state)
 {
-    relayState = !relayState;
-    // 更新继电器状态
-    digitalWrite(relayPin, relayState);
-    // 打印继电器状态
-    Serial.print("Relay state: ");
-    Serial.println(relayState);
+  relayState = !relayState;
+  // 更新继电器状态
+  digitalWrite(relayPin, relayState);
+  // 打印继电器状态
+  Serial.print("Relay state: ");
+  Serial.println(relayState);
 }
 
+// 生成彩虹颜色循环
+uint32_t Wheel(byte WheelPos)
+{
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85)
+  {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170)
+  {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
 
-void setup() {
+void initup(){
+
+}
+void setup()
+{
   Serial.begin(115200); // 初始化串口
   initws2812();
   startTime = millis(); // 记录开机时间
   u8g2.begin();
   u8g2.enableUTF8Print(); // enable UTF8 support for the Arduino print() function
-
   // 设置继电器引脚为输出模式
   pinMode(relayPin, OUTPUT);
-
+  Blinker.begin(auth, WiFi.SSID().c_str(), WiFi.psk().c_str()); // 初始化blinker
   // 初始继电器状态为关闭
   digitalWrite(relayPin, relayState);
 #if defined(BLINKER_PRINT)
   BLINKER_DEBUG.stream(BLINKER_PRINT);
 #endif
   u8g2.setFont(u8g2_font_wqy14_t_gb2312a);
-  connect();//连接WiFi
-  Blinker.begin(auth,WiFi.SSID().c_str(),WiFi.psk().c_str());//初始化blinker
+  connect();                                                    // 连接WiFi
   mode.attach(mode_callback);
   Slider1.attach(slider_callback);
   Color.attach(rgb1_callback);
 }
 
+void rainbow()
+{
+   uint16_t i, j;
+   for (j = 0; j < 256 * 5; j++) {  // 5个完整的彩虹循环
+    for (i = 0; i < pixels.numPixels(); i++) {
+     pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+  }
+}
+  pixels.show();
+}
 
-
-void loop(){
+void saveConfigCallback () {
+  page=2;
+}
+void loop()
+{
   Blinker.run();
-  uint8_t wlan=WiFi.status(); 
-  do {
-    if(progress<100){ 
-      if(wlan!=WL_CONNECTED)   
-      progress += 3;
-      else
-      progress=103;
-     drawProgressBar(progress);//显示进度条
-    }else if (progress==100){
-      wifiManager.startConfigPortal("vegds","12345678");//启动ap
+  bool isrun=false;
+  uint8_t wlan = WiFi.status();
+  unsigned long currentMillis = millis();
 
-    } else if (progress==103){
-      u8g2.clearDisplay();//清除屏幕
-      progress=104;
-    }else{
-      u8g2.clearBuffer();
-    unsigned long currentTime = millis();
-    unsigned long uptime = currentTime - startTime; // 计算已开机时间
-    unsigned long hours = uptime / 3600000;
-    unsigned long minutes = (uptime / 60000) % 60;
-    unsigned long seconds = (uptime / 1000) % 60;
-    char timeString[9];
-    sprintf(timeString, "%02lu:%02lu:%02lu", hours, minutes, seconds); // 格化时间字符串
-    // 在屏幕上显示已开机时间
-    u8g2.drawUTF8(0, 15, "启动时间:");
-    u8g2.setFont(u8g2_font_8x13_tr);
-    u8g2.drawStr(60, 15, timeString);
-    //显示hex颜色
-    u8g2.drawStr(30, 45, hex_color);  
-    //显示ip
-    u8g2.setFont(u8g2_font_wqy14_t_gb2312a);
-    u8g2.drawUTF8(0, 30,"IP:");
-    // 显示灯的状态
-    u8g2.drawUTF8(0, 60,"状态:");
-
-    if (relayState)
-      u8g2.drawStr(33,60,"ON ");
-    else
-    u8g2.drawStr(33,60,"OFF");
-
-    u8g2.drawUTF8(0, 45,"HEX:");
-
-    //判断是否联网
-    if(Blinker.connected()){
-    u8g2.drawUTF8(20, 30,WiFi.localIP().toString().c_str());
-  }else{
-    u8g2.drawUTF8(20, 30, "未连接到WiFi");
+  // 检查时间间隔是否达到
+  if (currentMillis - previousMillis >= 1000&&!change) {
+    rainbow();
+    previousMillis = currentMillis;
     }
-  } 
-}while (u8g2.nextPage());
 
+  if (progress < 100)
+    {
+      if (wlan != WL_CONNECTED){
+        progress += 6;
+        page=1;
+      }
+      else{
+        progress = 103;
+        page=2;
+      }
+    }
+  if (progress == 100)
+    {
+      page=3;
+      progress+= 6;
+    }
+  switch (page)
+  {
+  case 1:
+    u8g2.firstPage();
+    do
+    {
+      drawProgressBar(progress); // 显示进度条
+    } while (u8g2.nextPage());
+    break;
+  case 3:
+    do
+  {
+      u8g2.clearBuffer();
+      u8g2.drawUTF8(0, 15, "请连接到vegds");
+      u8g2.drawUTF8(0, 32, "密码为12345678");
+      u8g2.drawUTF8(0, 47, "配网成功后自动设置");
+      u8g2.sendBuffer();
+      page=4;
+      break;
+  } while (u8g2.nextPage());
+    break;
+
+    case 4:
+    if(!isrun){
+    wifiManager1.setSaveConfigCallback(saveConfigCallback);
+    wifiManager1.startConfigPortal("vegds","12345678");
+    isrun=true;
+    }
+    break;
+
+    case 2:
+    do{
+      u8g2.clearBuffer();
+      char* timeStr = new char[7]; // 6 characters for hhmmss + 1 for null terminato
+      time_t time = Blinker.runTime();
+      sprintf(timeStr, "%02d:%02d:%02d", hour(time), minute(time), second(time));
+      u8g2.drawUTF8(0, 15, "启动时间:");
+      u8g2.setFont(u8g2_font_8x13_tr);
+      u8g2.drawStr(60, 15, timeStr);
+      // 显示hex颜色
+      u8g2.drawStr(30, 45, hex_color);
+      // 显示ip
+      u8g2.setFont(u8g2_font_wqy14_t_gb2312a);
+      u8g2.drawUTF8(0, 30, "IP:");
+      // 显示灯的状态
+      u8g2.drawUTF8(0, 60, "状态:");
+
+      u8g2.drawUTF8(60, 60, "ver:12.3.7");
+      if (relayState)
+        u8g2.drawStr(33, 60, "ON");
+      else
+        u8g2.drawStr(33, 60, "OFF");
+
+      u8g2.drawUTF8(0, 45, "HEX:");
+
+      // 判断是否联网
+      if (Blinker.connected())
+      {
+        u8g2.drawUTF8(20, 30, WiFi.localIP().toString().c_str());
+      }
+      else
+      {
+        u8g2.drawUTF8(20, 30, "未连接到WiFi");
+      }
+
+    }while (u8g2.nextPage());
+    delay(1000);
+  }
 }
